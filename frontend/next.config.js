995bbd436a20bc10adbc0158fe2a2f175ed4fc73
Tanishk Sinha <tanishksinhasw007@gmail.com>
2026-05-18 03:05:21 +0530

    fix: disable webpack filesystem caching in next.config.js to allow running on drives with zero space

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     // Allow optimized images from local sources
     unoptimized: false,
   },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
